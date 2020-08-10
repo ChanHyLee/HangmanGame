de5a2b5a0f2f25d1464dde41778a28c6f8e8a88f
ChanHyLee <65821042+ChanHyLee@users.noreply.github.com>
2020-08-10 23:43:34 +0900

    Add files via upload

---
+++ js/intro.js
@@ -0,0 +1,96 @@
+const openingWrap = document.querySelector(".js-openingWrap");
+const wordInput = document.querySelector(".js-wordInput");
+const hangman = document.querySelector(".js-hangmanImg");
+const hangman2 = document.querySelector(".js-hangmanImg2");
+
+const SHOWING_CLASS = "showing";
+
+let answer = "";
+
+function handleEnterUp(event) {
+    hangman.classList.add(SHOWING_CLASS);
+    hangman2.classList.remove(SHOWING_CLASS);
+    const word = wordInput.value;
+    if (word !== "") {
+        let count = 0;
+        for (let i = 0; i < word.length; i++) {
+            if ((word.charCodeAt(i) >= 65 && word.charCodeAt(i) <= 90) || (word.charCodeAt(i) >= 97 && word.charCodeAt(i) <= 122)) {
+                count = count + 1;
+            }
+        }
+        if (count === word.length) {
+            answer = word;
+            wordInput.value = "";
+            wordInput.blur();
+            openingWrap.classList.remove(SHOWING_CLASS);
+            console.log(answer);
+        } else {
+            wordInput.value = "";
+            alert("Please write words using only the alphabet.");
+        }
+    } else {
+        wordInput.value = "";
+        alert("You didn't write anything!");
+    }
+}
+
+function handleEnterDown(event) {
+    hangman.classList.remove(SHOWING_CLASS);
+    hangman2.classList.add(SHOWING_CLASS);
+}
+
+function handleMouseUp(event) {
+    const word = wordInput.value;
+    if (word !== "") {
+        let count = 0;
+        for (let i = 0; i < word.length; i++) {
+            if ((word.charCodeAt(i) >= 65 && word.charCodeAt(i) <= 90) || (word.charCodeAt(i) >= 97 && word.charCodeAt(i) <= 122)) {
+                count = count + 1;
+            }
+        }
+        if (count === word.length) {
+            answer = word;
+            wordInput.value = "";
+            wordInput.blur();
+            openingWrap.classList.remove(SHOWING_CLASS);
+            console.log(answer);
+        } else {
+            wordInput.value = "";
+            alert("Please write words using only the alphabet.");
+        }
+    } else {
+        wordInput.value = "";
+        alert("You didn't write anything!");
+    }
+}
+
+function handleMouseOut(event) {
+    hangman.classList.add(SHOWING_CLASS);
+    hangman2.classList.remove(SHOWING_CLASS);
+}
+
+function handleMouseOver(event) {
+    hangman.classList.remove(SHOWING_CLASS);
+    hangman2.classList.add(SHOWING_CLASS);
+}
+
+function init() {
+    openingWrap.classList.add(SHOWING_CLASS);
+    hangman.classList.add(SHOWING_CLASS);
+    hangman2.classList.remove(SHOWING_CLASS);
+    hangman.addEventListener("mouseover", handleMouseOver);
+    hangman2.addEventListener("mouseout", handleMouseOut);
+    hangman2.addEventListener("mouseup", handleMouseUp);
+    wordInput.addEventListener("keydown", function(event) {
+        if (event.keyCode === 13) {
+            handleEnterDown();
+        }
+    });
+    wordInput.addEventListener("keyup", function(event) {
+        if (event.keyCode === 13) {
+            handleEnterUp();
+        }
+    });
+    wordInput.focus();
+}
+init();
